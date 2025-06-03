f6324b70ffbb9fcec7886ce03f11b7f937a62a4c
Krissz91 <krisztian.toth2.0@gmail.com>
2025-06-03 15:29:42 +0200

    update

--- createalinuxuser.py
+++ createalinuxuser.py
@@ -2,42 +2,43 @@
 import os
 
 # Request input
-username = input("Add meg a felhasználó nevét: ").strip()
-groupname = input("Add meg a csoport nevét: ").strip()
+username = input("Enter the user's name: ").strip()
+groupname = input("Enter the group's name: ").strip()
 
-print(f"\nEllenőrzés: {username} felhasználó létezik-e...")
+print(f"\nChecking: if user {username} exists...")
 
-# Does the user exist?
+# Does the user exists?
+# With the command > /dev/null it tells the program to direct any output to this so it doesn't come up on the screen and the 2>&1 tells it to direct all errors (2) to the same place you told it to put the output from the first command.
 exitcode = os.system(f"id {username} > /dev/null 2>&1")
 if exitcode != 0:
-    print(f"A(z) {username} felhasználó nem létezik. Létrehozás...")
+    print(f"The user {username} does not exist. Creating...")
     os.system(f"useradd {username}")
 else:
-    print(f"A(z) {username} felhasználó már létezik, kihagyva.")
+    print(f"User {username} already exists, omitted.")
 
 # Check and create a group
 exitcode = os.system(f"getent group {groupname} > /dev/null 2>&1")
 if exitcode != 0:
-    print(f"A(z) '{groupname}' csoport nem létezik. Létrehozás...")
+    print(f"Group '{groupname}' does not exist. Creating...")
     os.system(f"groupadd {groupname}")
 else:
-    print(f"'{groupname}' csoport már létezik, kihagyva.")
+    print(f"Group '{groupname}' already exists, skipped.")
 
 # Add user to the specified group
-print(f"Felhasználó '{username}' hozzáadása a '{groupname}' csoporthoz...")
+print(f"Adding user '{username}' to group '{groupname}'...")
 os.system(f"usermod -aG {groupname} {username}")
 
 # Create a library
 dir_path = f"/opt/{groupname}_dir"
 if not os.path.isdir(dir_path):
-    print(f"Könyvtár létrehozása: {dir_path}")
+    print(f"Create a library: {dir_path}")
     os.mkdir(dir_path)
 else:
-    print(f"Könyvtár már létezik: {dir_path}, kihagyva.")
+    print(f"Library already exists: {dir_path}, omitted.")
 
 # Setting permissions
-print("Jogosultságok és tulajdonos beállítása...")
+print("Setting permissions and owner...")
 os.system(f"chown :{groupname} {dir_path}")
 os.system(f"chmod 770 {dir_path}")
 
-print("\nKész.")
+print("\nReady.")
